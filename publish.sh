c53065e22aec70522f4b775906129cd2bb9edc0c
Eric Bower <me@erock.io>
2023-08-11 13:52:06 -0400

    blah

--- publish.sh
+++ publish.sh
@@ -1,13 +1,8 @@
-#!/bin/sh -l
+#!/bin/sh
+set -eu
+
 ssh -V
 
 echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
-
 chmod 600 __TEMP_INPUT_KEY_FILE
-
-scp -v -s \
-  -o IdentitiesOnly=yes \
-  -o StrictHostKeyChecking=no \
-  -i __TEMP_INPUT_KEY_FILE \
-  -r ${INPUT_SRC} \
-  "${INPUT_USER}"@pgs.sh:/"${INPUT_PROJECT}"
+scp -v -s -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i __TEMP_INPUT_KEY_FILE -r ${INPUT_SRC} "${INPUT_USER}"@pgs.sh:/"${INPUT_PROJECT}"
