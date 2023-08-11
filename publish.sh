35c6d4343860fad6ef56f04a49160084cc214dd9
Eric Bower <me@erock.io>
2023-08-11 13:35:53 -0400

    fix: gha

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh -l
 
-echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
+echo ssh -V
+
+echo -e "${INPUT_KEY}" > __TEMP_INPUT_KEY_FILE
 
 chmod 600 __TEMP_INPUT_KEY_FILE
 
-scp -s -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -r ${INPUT_SRC} "${INPUT_USER}"@pgs.sh:/"${INPUT_PROJECT}"
+scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -r ${INPUT_SRC} "${INPUT_USER}"@pgs.sh:/"${INPUT_PROJECT}"
