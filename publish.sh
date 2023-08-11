ea2caaed5fa575ccf3351898f08ad305e670dd36
Eric Bower <me@erock.io>
2023-08-11 13:41:46 -0400

    fix

--- publish.sh
+++ publish.sh
@@ -7,4 +7,9 @@ echo -e "${INPUT_KEY}" > __TEMP_INPUT_KEY_FILE
 
 chmod 600 __TEMP_INPUT_KEY_FILE
 
-scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -r ${INPUT_SRC} "${INPUT_USER}"@pgs.sh:/"${INPUT_PROJECT}"
+scp -v -s \
+  -o IdentitiesOnly=yes \
+  -o StrictHostKeyChecking=no \
+  -i __TEMP_INPUT_KEY_FILE \
+  -r ${INPUT_SRC} \
+  "${INPUT_USER}"@pgs.sh:/"${INPUT_PROJECT}"
